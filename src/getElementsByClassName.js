6e6a57cc4ac495cf11a11ac306c7e136e49fe8a6
Alex Cai <yhcai@ucdavis.edu>
2020-02-03 19:43:15 -0800

    finished

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,35 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+//    var body = document.body;
+//    var output = [];
+//    var findclassbyClass = function(element){
+//     if(element.classlist && elementclasslist.contain(className)){
+//         output.push(element);
+//        if(element.childnode){
+//             for(var i = 0 ; i < element.childnode.length; i++){
+//                 output.concat(findclassbyClass(element.childnode[i]));
+//             }
+//        } 
+    
+//     }
+//         return output;
+//    }
+
+//    return findclassbyClass(body);
+var docBody = document.body;
+  var output = [];
+  var findElementByClass = function(element){
+    if(element.classList && element.classList.contains(className)){
+      output.push(element);}
+      if(element.childNodes){
+        for(var i = 0; i < element.childNodes.length; i++){
+          output.concat(findElementByClass(element.childNodes[i]));
+        }
+      }
+      return output;
+    };
+    return findElementByClass(docBody);
+
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,56 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if( typeof obj === 'number')  {
+    return obj.toString();
+  }
+  if( obj === null){
+    return 'null';
+  }
+  if(typeof obj === 'boolean'){
+    if( obj === true){
+      return 'true';
+    }else{
+      return 'false';
+    }
+  }
+  if(typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  // if( obj === []){
+  //   return "[]";
+  // }
+   //recursion for obj
+   if(Array.isArray(obj) === false) {
+    if(typeof obj === 'object' && obj !== null) {
+      if (Object.keys(obj).length === 0) {
+      result = '{}';
+    } else {
+      var result = '{';
+      for (var key in obj) {
+        if (obj[key] === 'undefined' || typeof obj[key] === 'function') {
+          return '{}';
+        }
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+      result = result.slice(0, result.length -1);
+      result = result + '}';
+     }
+    }
+    }
+  
+    //recursion for array
+    //check if the obj is array
+    if(Array.isArray(obj)) {
+      var result = '[';
+      for (var i = 0; i < obj.length ; i++) {
+        result += stringifyJSON(obj[i])
+        if(i !== obj.length - 1) {
+          result += ',';
+        }
+      }
+      result += ']';
+    }
+    return result;
+
 };
